a9ece6b6674399f2193efd7b3c1306e538b6acea
Geunsik Lim <leemgs@users.noreply.github.com>
2018-02-04 23:07:58 +0900

    Create linked-list-remove-node.c

---
+++ linked-list-remove-node.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>    // malloc, free 함수가 선언된 헤더 파일
+
+struct NODE {    // 연결 리스트의 노드 구조체
+	struct NODE *next;    // 다음 노드의 주소를 저장할 포인터
+	int data;             // 데이터를 저장할 멤버
+};
+
+void addFirst(struct NODE *target, int data)    // Head라는 기준 노드 뒤에 노드를 추가하는 함수
+{
+	struct NODE *newNode = (struct NODE*) malloc(sizeof(struct NODE));    // 새 노드 생성
+	newNode->next = target->next;    // 새 노드의 다음 노드에 기준 노드의 다음 노드를 지정
+	newNode->data = data;            // 데이터 저장
+
+	target->next = newNode;          // 기준 노드의 다음 노드에 새 노드를 지정
+}
+
+void addLast(struct NODE *target, int data) //  제일 오른쪽 노트 뒤에 노드를 추가하는 함수 
+{
+	struct NODE *newNode = (struct NODE*) malloc(sizeof(struct NODE));
+	newNode->next = NULL; // 새로운 노드가  가리키는 노드가 더이상 없을것이므로 NULL을 명시함. 
+	newNode->data = data; // 새로운 노드에 새로운 데이터를 저장
+
+	struct NODE *temp = target; 
+	while (temp->next != NULL) // 마지막 노드를 찾는 루프
+	{
+		temp = temp->next; 
+	}
+	temp->next = newNode; // 새로 생성한 노드를 제일 마지막 노드에 연결
+
+}
+
+void removeNode(struct NODE *target, int data) //  동일한 데이타 값이 있는 노드를 찾아서 삭제하는 함수 
+{
+	struct NODE *prev = target;
+	struct NODE *temp = target;
+	while (temp->next != NULL) // 마지막 노드까지 순차 검색
+	{
+		prev = temp;  // 이전 노드의 정보를 기억하고 있어야 한다. 
+		temp = temp->next;
+		if (temp->data == data){ // 동일한 데이타 값을 가지는 노드인지 체크하기 
+			prev->next = temp->next;  // 동일한 데이타 값을 가지는 노드의 연결(link)을 끊어버리자. 
+			break; // 동일한 데이타 값을 가지는 노드를 찾았다면 while문은 빠져나가자.!!!
+		}
+	}
+}
+
+int main(void)
+{
+	struct NODE *head = (struct NODE*) malloc(sizeof(struct NODE));    // 머리 노드 생성
+	head->next = NULL; 	// 주의: 머리 노드는 데이터를 저장하지 않음.이유는 Pointer(*) 프로그래밍이기 때문임. 
+
+	int node_num = 10;
+	for (int i = 1; i <= node_num; i++){
+		//addFirst(head, i*10);        // 머리 노드 뒤에 새 노드 추가
+		addLast(head, i*10);           // 가장 오른쪽에 노드를 새 노드 추가 
+	}
+
+	for (int i = 1; i <= node_num; i++){
+		removeNode(head, 50); 
+	}
+	struct NODE *curr = head->next;    // 연결 리스트 순회용 포인터에 첫 번째 노드의 주소 저장
+	while (curr != NULL)               // 포인터가 NULL이 아닐 때 계속 반복
+	{
+		printf("%d\n", curr->data);    // 현재 노드의 데이터 출력
+		curr = curr->next;             // 포인터에 다음 노드의 주소 저장
+	}
+
+	curr = head->next;      // 연결 리스트 순회용 포인터에 첫 번째 노드의 주소 저장
+	while (curr != NULL)    // 포인터가 NULL이 아닐 때 계속 반복
+	{
+		struct NODE *next = curr->next;    // 현재 노드의 다음 노드 주소를 임시로 저장
+		free(curr);                        // 현재 노드 메모리 해제
+		curr = next;                       // 포인터에 다음 노드의 주소 저장
+	}
+
+	free(head);    // 머리 노드 메모리 해제
+
+	return 0;
+}
